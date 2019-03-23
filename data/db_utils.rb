889521ed8ffcea078d856cf377aec74c19e95fd4
Michael Barlow <m.barlow12@gmail.com>
2019-03-23 15:04:18 -0600

    model update, csv added, table creation in db_utils

--- data/db_utils.rb
+++ data/db_utils.rb
@@ -1,5 +1,8 @@
+require "sequel"
+require "csv"
+require "date"
+
 module Connection
-  require "sequel"
 
   def self.config
     {
@@ -17,5 +20,31 @@ module Connection
     rescue Sequel::Error => e
       puts e.message
     end
+  end
+end
+
+module Db
+
+  def self.init(cfg)
+    Sequel.postgres(cfg) do |db|
+      db.create_table?(:people) do
+        primary_key :id
+        String :name, :unique => true
+      end
+
+      db.create_table?(:pizzas) do
+        primary_key :id
+        String :type
+        foreign_key :person_id, :people
+        DateTime :eaten_at
+      end
+    end
+  end
+
+  def self.load_from_file(path)
+    csv = CSV.read(path, :headers => true)
+    csv.map { |row|
+
+    }
   end
 end
--- data/models.rb
+++ data/models.rb
@@ -1,24 +1,10 @@
 require 'sequel'
 require 'db_utils'
 
-DB = Sequle.postgres(Connection.config)
-
-Sequel::Model.plugin :schema
+DB = Sequel.postgres(Connection.config)
 
 class Person < Sequel::Model
-
-  set_schema do
-    primary_key :id
-    String :name
-  end
-
-  create_table unless table_exists?
 end
 
 class Pizza < Sequel::Model
-
-  set_schema do
-    primary_key :id
-    String :type
-    foreign_key :person_id, 
-  end
+end
